5d59fa6cd1f463a44aa550285cb5354405909234
Erin <erin.fuchsia@gmail.com>
2019-05-01 15:08:34 -0400

    made a dropdown

--- Assets/Scripts/Monster Shooting AR code/FamilyContentController.cs
+++ Assets/Scripts/Monster Shooting AR code/FamilyContentController.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.UI;
+using TMPro;
+
+
 
 public class FamilyContentController : MonoBehaviour {
+
+    //for the dropdown version
+   
 
     public GameObject familyContentObject;
     private List<string> familyNames;
@@ -15,8 +21,14 @@ public class FamilyContentController : MonoBehaviour {
     }
     public void FamilyContentPosition()
     {
+
+        var test = gameObject.GetComponent<TMP_Dropdown>();
+        var thisopts = test.options;
+        var thisval = test.value;
+        var thisname = thisopts[thisval].text.ToString();
         familyNames = GameObject.Find("Canvas").GetComponent<FindAddedSugar>().fms;
-        GameObject tle = GameObject.Find(this.name + " Monsters Title");
+        //GameObject tle = GameObject.Find(this.name + " Monsters Title");
+        GameObject tle = GameObject.Find(thisname + " Monsters Title");
         float titleHeight = tle.GetComponent<RectTransform>().rect.height;
         Vector2 newPosition = new Vector2(familyContentObject.GetComponent<RectTransform>().localPosition.x, Math.Abs(tle.GetComponent<RectTransform>().localPosition.y) - titleHeight);
         familyContentObject.GetComponent<RectTransform>().localPosition = newPosition;
