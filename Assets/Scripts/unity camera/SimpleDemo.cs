44f7f9e15f4ed11d520a2708077a6743016fb4c4
ZakirChaudry <zakirchaudry@gmail.com>
2019-05-07 15:10:28 -0400

    unsquashed the camera

--- Assets/Scripts/unity camera/SimpleDemo.cs
+++ Assets/Scripts/unity camera/SimpleDemo.cs
@@ -81,12 +81,20 @@ public class SimpleDemo : MonoBehaviour
             //var newHeight = rect.sizeDelta.x * BarcodeScanner.Camera.Height / BarcodeScanner.Camera.Width;
             //rect.sizeDelta = new Vector2(rect.sizeDelta.x, newHeight);
             //rect.sizeDelta = new Vector2(Screen.width, Screen.height);
+           
         };
 
-        // Track status of the scanner
-        //BarcodeScanner.StatusChanged += (sender, arg) => {
-        //  Debug.Log("Status: " + BarcodeScanner.Status);
-        //};
+
+
+        BarcodeScanner.StatusChanged += (sender, arg) =>
+        {
+            RectTransform r = Image.GetComponent<RectTransform>();
+            //r.sizeDelta = new Vector2 (BarcodeScanner.Camera.Width, BarcodeScanner.Camera.Height);
+            float w = BarcodeScanner.Camera.Width;
+            float h = BarcodeScanner.Camera.Height;
+            AspectRatioFitter a = Image.GetComponent<AspectRatioFitter>();
+            a.aspectRatio = w / h;
+        };
 
         isAndroid = false;
         //When on Android platform
