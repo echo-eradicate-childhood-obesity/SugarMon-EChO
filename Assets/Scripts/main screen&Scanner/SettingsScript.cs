637038b6bcfb41f67466196159d2f5f13544c242
Mark <markbotaish@gmail.com>
2019-05-15 11:14:08 -0400

    Menu
    - Added DoNotDestoryOnLoad Settings
    - Added "soft" corners

---
+++ Assets/Scripts/main screen&Scanner/SettingsScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsScript : MonoBehaviour {
+
+    public static SettingsScript instance;
+
+    private bool hasSound = true;
+    private bool hasVibration = true;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void toggleSound() { hasSound = !hasSound; }
+    public void toggleVibration() { hasVibration = !hasSound; }
+    public void setSoundSettings(bool tof) { hasSound = tof; }
+    public void setVirbrationSettings(bool tof) { hasVibration = tof; }
+
+    public bool getSoundSettings(){ return hasSound;}
+    public bool getVibrationSettings(){ return hasVibration; }
+}
