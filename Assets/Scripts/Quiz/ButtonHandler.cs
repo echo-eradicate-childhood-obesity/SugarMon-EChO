6349816ff69def8d32d07dd8bbb0b6be35cf72f1
Aidan Lee <aidanwlee@gmail.com>
2019-05-17 10:41:13 -0400

    initial quiz scene

---
+++ Assets/Scripts/Quiz/ButtonHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandler : MonoBehaviour {
+    private Question question;
+    // Initialize Quiz with the first question
+    void Start() {
+        question = new Question();
+        GameObject.Find("Current Question Text Box").GetComponentInChildren<Text>().text = question.prompt;
+        GameObject.Find("Option A Button").GetComponentInChildren<Text>().text = question.answerA;
+        GameObject.Find("Option B Button").GetComponentInChildren<Text>().text = question.answerB;
+        GameObject.Find("Option C Button").GetComponentInChildren<Text>().text = question.answerC;
+        GameObject.Find("Option D Button").GetComponentInChildren<Text>().text = question.answerD;
+    }
+    // Sets the text of Option A Button
+    public void ButtonClicked(string ButtonLabel) {
+        if(ButtonLabel[0] == question.correct) {
+            Debug.Log("Correct!");
+        }
+        else {
+            Debug.Log("Wrong");
+        }
+    }
+}
---
+++ Assets/Scripts/Quiz/Question.cs
@@ -0,0 +1,21 @@
+﻿
+public class Question {
+    public string prompt{get; set;}
+    public string answerA{get; set;}
+    public string answerB{get; set;}
+    public string answerC{get; set;}
+    public string answerD{get; set;}
+    public char correct{get; set;}
+
+    public Question() {
+        generateQuestion();
+    }
+    private void generateQuestion() {
+        prompt = "Which of the following is NOT a type of sugar?";
+        answerA = "Canes";
+        answerB = "Concentrates";
+        answerC = "Extracts";
+        answerD = "OSEs";
+        correct = 'C';
+    }
+}
