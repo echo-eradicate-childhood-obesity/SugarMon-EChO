3d5c36a5438f694aaa887d579596f3b38573f465
Aidan Lee <aidanwlee@gmail.com>
2019-05-17 14:05:07 -0400

    fixed fade animations

--- Assets/Scripts/Quiz/ButtonHandler.cs
+++ Assets/Scripts/Quiz/ButtonHandler.cs
@@ -6,12 +6,21 @@ using UnityEngine.UI;
 public class ButtonHandler : MonoBehaviour {
     private Question question;
     private bool wasSkipped;
+    private Text prompt;
+    private Text textA;
+    private Text textB;
+    private Text textC;
+    private Text textD;
+
+    private Color Default = new Color(255, 255, 255, 255);
+    private Color Correct = new Color(155, 255, 155, 255);
+    private Color Incorrect = new Color(255, 155, 155, 255);
+
     // Initialize Quiz with the first question
     IEnumerator Start() {
         question = new Question();
         wasSkipped = false;
         float waitCounter;
-
         for(int i = 0; i <= question.prompt.Length; i++) {
             GameObject.Find("Current Question Text Box").GetComponentInChildren<Text>().text = question.prompt.Substring(0, i);
             waitCounter = 0f;
@@ -22,53 +31,70 @@ public class ButtonHandler : MonoBehaviour {
             }
         }
         waitCounter = 0f;
-        while (waitCounter < 1.5f && wasSkipped == false) {
+        while (waitCounter < 1f && wasSkipped == false) {
             if (Input.GetButtonDown("Fire1") || Input.touchCount > 0) wasSkipped = true; // if screen tapped, end timer
             yield return null;
             waitCounter += Time.deltaTime; // update timer
         }
-        GameObject.Find("Option A Button").GetComponentInChildren<Text>().text = question.answerA;
+        textA = GameObject.Find("A Button").GetComponentInChildren<Text>();
+        textA.text = question.answerA;
+        StartCoroutine(FadeTextIn(2f, textA));
 
         waitCounter = 0f;
-        while (waitCounter < 1.5f && wasSkipped == false) {
+        while (waitCounter < 1f && wasSkipped == false) {
             if (Input.GetButtonDown("Fire1") || Input.touchCount > 0) wasSkipped = true; // if screen tapped, end timer
             yield return null;
             waitCounter += Time.deltaTime; // update timer
         }
-        GameObject.Find("Option B Button").GetComponentInChildren<Text>().text = question.answerB;
+        textB = GameObject.Find("B Button").GetComponentInChildren<Text>();
+        textB.text = question.answerB;
+        StartCoroutine(FadeTextIn(2f, textB));
 
         waitCounter = 0f;
-        while (waitCounter < 1.5f && wasSkipped == false) {
+        while (waitCounter < 1f && wasSkipped == false) {
             if (Input.GetButtonDown("Fire1") || Input.touchCount > 0) wasSkipped = true; // if screen tapped, end timer
             yield return null;
             waitCounter += Time.deltaTime; // update timer
         }
-        GameObject.Find("Option C Button").GetComponentInChildren<Text>().text = question.answerC;
+        textC = GameObject.Find("C Button").GetComponentInChildren<Text>();
+        textC.text = question.answerC;
+        StartCoroutine(FadeTextIn(2f, textC));
 
         waitCounter = 0f;
-        while (waitCounter < 1.5f && wasSkipped == false) {
+        while (waitCounter < 1f && wasSkipped == false) {
             if (Input.GetButtonDown("Fire1") || Input.touchCount > 0) wasSkipped = true; // if screen tapped, end timer
             yield return null;
             waitCounter += Time.deltaTime; // update timer
         }
-        GameObject.Find("Option D Button").GetComponentInChildren<Text>().text = question.answerD;
+        textD = GameObject.Find("D Button").GetComponentInChildren<Text>();
+        textD.text = question.answerD;
+        StartCoroutine(FadeTextIn(2f, textD));
     }
     // Responds to the press of any button
     public void ButtonClicked(string ButtonLabel) {
         if(ButtonLabel[0] == question.correct) { // correct button pressed
             Debug.Log("Correct!");
+            Button tempButton = GameObject.Find(ButtonLabel + " Button").GetComponentInChildren<Button>();
+            ColorBlock newColors = tempButton.colors;
+            newColors.normalColor = Correct;
+            tempButton.colors = newColors;
         }
         else { // wrong button pressed
             Debug.Log("Wrong");
+            Button tempButton = GameObject.Find(ButtonLabel + " Button").GetComponentInChildren<Button>();
+            ColorBlock newColors = tempButton.colors;
+            newColors.normalColor = Incorrect;
+            tempButton.colors = newColors;
         }
     }
-    // DOES NOT WORK
-    IEnumerator waitWithSkip(float seconds) {
+
+    public IEnumerator FadeTextIn(float timer, Text text) {
         float waitCounter = 0f;
-        while (waitCounter < seconds && wasSkipped == false) {
-            if (Input.GetButtonDown("Fire1") || Input.touchCount > 0) wasSkipped = true; // if screen tapped, end timer
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 0);
+        while (waitCounter < timer) {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a + (waitCounter / timer));
             yield return null;
-            waitCounter += Time.deltaTime; // update timer
+            waitCounter += Time.deltaTime;
         }
     }
 }
