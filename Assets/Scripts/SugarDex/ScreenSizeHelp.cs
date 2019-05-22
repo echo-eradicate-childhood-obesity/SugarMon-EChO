ac871d23a95c0e9fe43e72977f32cf683b93f432
ZakirChaudry <zakirchaudry@gmail.com>
2019-05-22 15:54:28 -0400

    Created new SugarDex Scene and added scrollview

---
+++ Assets/Scripts/SugarDex/ScreenSizeHelp.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class ScreenSizeHelp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        CanvasScaler cs = this.transform.GetComponent<CanvasScaler>();
+        cs.referenceResolution = new Vector2(Screen.width, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
