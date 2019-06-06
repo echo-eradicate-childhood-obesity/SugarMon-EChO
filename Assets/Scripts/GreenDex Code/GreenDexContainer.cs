d1e95f2c67e2c05a425b8d012fe57c1f6f0011d1
Aidan Lee <aidanwlee@gmail.com>
2019-06-06 15:29:07 -0400

    added click name to go to detail page

--- Assets/Scripts/GreenDex Code/GreenDexContainer.cs
+++ Assets/Scripts/GreenDex Code/GreenDexContainer.cs
@@ -12,29 +12,21 @@ public class GreenDexContainer : MonoBehaviour {
     public GameObject pName; // name of the food scanned
     public GameObject pIcon; // green check / red exclamation
     public GameObject pTimeDate; // time and date when scaned
-    public GameObject pFrame; // outline around each item in list
 
     TextMeshProUGUI pText;
     TextMeshProUGUI pTimeDateDisplay;
-    Button pFrameButton;
     Image pImage;
 
 	void Start () {
         pImage = pIcon.GetComponent<Image>();
-        pFrameButton = pFrame.GetComponent<Button>();
         pText = pName.GetComponent<TextMeshProUGUI>();
         pTimeDateDisplay = pTimeDate.GetComponent<TextMeshProUGUI>();
-        pFrameButton.onClick.AddListener(() => ClearEdit()); // If anywhere on the screen is clicked close edit mode
     }
 
     void Update () {
         pText.text = pi.GetDisplayName();
         pImage.sprite = pi.GetSprite();
         pTimeDateDisplay.text = pi.displayDateTime();
-    }
-
-    private void ClearEdit() {
-        GreenCartController.Instance.editMode = false;
     }
 
     private void SetPi(Category cate) {
